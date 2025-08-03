cf709a5c72fe533b30180b4f305788fa0d0e0972
Maxim <maxim.dudaryonok@yandex.ru>
2025-08-04 00:29:24 +0300

    fix: router provider

--- src/app/providers/routerProvider/routeProvider.tsx
+++ src/app/providers/routerProvider/routeProvider.tsx
@@ -1,91 +1,43 @@
 import {
   createBrowserRouter,
-  isRouteErrorResponse,
   RouterProvider,
-  useRouteError,
 } from 'react-router-dom';
 import { Paths } from 'models/routerTypes';
-import { SearchPage, Layout } from 'pages';
+import { SearchPage, Layout, Hero, NotFound, About } from 'pages';
 import { ErrorElement } from '../../../components/ErrorBoundary/ui/ErrorElement.tsx';
-import { GetData } from '../../../utils/api/get-data.ts';
-
-function ErrorBoundaryWrapper() {
-  const error = useRouteError();
-
-  let errorMessage: string;
-
-  if (isRouteErrorResponse(error)) {
-    errorMessage = `${error.status} ${error.statusText}`;
-  } else if (error instanceof Error) {
-    errorMessage = error.message;
-  } else if (typeof error === 'string') {
-    errorMessage = error;
-  } else {
-    errorMessage = 'Unknown error';
-  }
-
-  return <ErrorElement errorInfo={errorMessage} />;
-}
+import type { FC } from 'react';
 
 const router = createBrowserRouter([
   {
     path: Paths.base,
     element: <Layout />,
-    errorElement: <ErrorBoundaryWrapper />,
+    errorElement: <ErrorElement />,
     children: [
       {
         path: Paths.base,
         element: <SearchPage />,
-        loader: async ({ request }) => {
-          const url = new URL(request.url);
-          const query = url.searchParams.get('query') ?? '';
-          const page = parseInt(url.searchParams.get('page') ?? '1', 10);
-          const data = await GetData(query, page);
-
-          return { data, query, page };
-        },
         children: [
           {
             path: `${Paths.hero}:id`,
-            lazy: async () => {
-              const { Hero } = await import('pages/index.ts');
-
-              return {
-                Component: Hero,
-                errorElement: <ErrorBoundaryWrapper />,
-              };
-            },
+            element: <Hero />,
+            errorElement: <ErrorElement />,
           },
         ],
       },
       {
         path: Paths.notFound,
-        lazy: async () => {
-          const { NotFound } = await import('pages/index.ts');
-
-          return {
-            Component: NotFound,
-            errorElement: <ErrorBoundaryWrapper />,
-          };
-        },
+        element: <NotFound />,
       },
       {
         path: Paths.about,
-        lazy: async () => {
-          const { About } = await import('pages/index.ts');
-
-          return {
-            Component: About,
-            errorElement: <ErrorBoundaryWrapper />,
-          };
-        },
+        element: <About />,
       },
     ],
   },
 ]);
 
-export const RouteProvider = () => {
+const RouteProvider: FC = () => {
   return <RouterProvider router={router} />;
 };
 
-export { router };
+export { RouteProvider };
